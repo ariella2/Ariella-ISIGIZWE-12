0cf5f70e4ae99346a66cd1440993773dbc0a46a7
Ariella <isingizweariella@gmail.com>
2023-05-29 22:57:33 +0200

    ok

--- connection2.php
+++ connection2.php
@@ -5,8 +5,10 @@ $username="root";
 $password="";
 $db_name="fc_games";
 
+$conn = new mysqli($servername, $username, $password, $dbname);
+
 if($conn->connection_error){
-    die("connection failed".$conn->connection_error);
+    die("connection failed". $conn->connection_error);
 }
 
 echo"connection successful";
